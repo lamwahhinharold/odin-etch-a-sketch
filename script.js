4e75f460711c00c880a364ef21d966381df77942
Harold Lam <lamwahhinharold@gmail.com>
2025-08-22 23:24:59 +0800

    Add button to ask for new grid size

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-const gridSize = 16;
 const container = document.querySelector("#container");
+const button = document.querySelector("#btn");
 
-// create the square divs
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+function createSquareDivs(gridSize) {
+  container.replaceChildren();
+  container.style.setProperty("--grid-size", gridSize);
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
 }
 
-// color the square when hover
-container.addEventListener("mouseover", (event) => {
-  if (event.target.classList.contains("square")) {
-    event.target.classList.add("colored");
-  }
-});
+function enableHoverColoring() {
+  container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("square")) {
+      event.target.classList.add("colored");
+    }
+  });
+}
+
+function setUpButton() {
+  button.addEventListener("click", () => {
+    const userInputGridSize = Number(
+      prompt(
+        "Please enter the number of squares per side for the new grid (MAX 100):"
+      )
+    );
+
+    if (isNaN(userInputGridSize) || userInputGridSize <= 0) {
+      alert("Please enter a valid positive number!");
+    } else if (userInputGridSize > 100) {
+      alert("Please enter a smaller number (MAX 100).");
+    } else {
+      createSquareDivs(userInputGridSize);
+    }
+  });
+}
+
+function init() {
+  createSquareDivs(16);
+  enableHoverColoring();
+  setUpButton();
+}
+
+init();
