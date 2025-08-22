b4cd978333e4d097042869c297d8540630425b05
Harold Lam <lamwahhinharold@gmail.com>
2025-08-22 22:00:08 +0800

    Add hover effect to color square divs on mouseover

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const gridSize = 16;
 const container = document.querySelector("#container");
 
+// create the square divs
 for (let i = 0; i < gridSize * gridSize; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
   container.appendChild(square);
 }
+
+// color the square when hover
+container.addEventListener("mouseover", (event) => {
+  if (event.target.classList.contains("square")) {
+    event.target.classList.add("colored");
+  }
+});
