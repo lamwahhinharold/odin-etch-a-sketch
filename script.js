5e6b6cc430679ec98f115cd0354c690f49878ba5
Harold Lam <lamwahhinharold@gmail.com>
2025-08-22 21:24:58 +0800

    Add 16x16 grid of square divs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-for (let i = 0; i < 16 * 16; i++) {
-  const container = document.querySelector("#container");
+const gridSize = 16;
+const container = document.querySelector("#container");
 
+for (let i = 0; i < gridSize * gridSize; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
   container.appendChild(square);
